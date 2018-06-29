52e5913cce38051c18d3b0fadb6a549bcef458c7
Adam Kosmala <adam.kosmala@cognifide.com>
2018-06-29 14:27:20 +0200

    - fixed formatting - splitChunks configuration added

--- template/webpack.prod.js
+++ template/webpack.prod.js
@@ -7,7 +7,6 @@ const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const base = require('./webpack.common')({
     mode: 'production'
 });
-
 module.exports = Object.assign(
     {},
     base,
@@ -28,24 +27,34 @@ module.exports = Object.assign(
         devtool: 'none',
         optimization: {
             mangleWasmImports: true,
+            splitChunks: {
+                chunks: 'all',
+                cacheGroups: {
+                    vendors: {
+                        name: 'dist/vendors',
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10
+                    },
+                }
+            },
             minimizer: [
-              new UglifyJsPlugin({
-                sourceMap: true,
-                uglifyOptions: {
-                  ecma: 6,
-                  mangle: true,
-                },
-              }),
-              new OptimizeCssAssetsPlugin({
-                assetNameRegExp: /\.css$/g,
-                cssProcessor: cssnano,
-                cssProcessorOptions: {
-                  discardComments: {
-                    removeAll: false,
-                  },
-                },
-                canPrint: true,
-              }),
+                new UglifyJsPlugin({
+                    sourceMap: true,
+                    uglifyOptions: {
+                        ecma: 6,
+                        mangle: true,
+                    },
+                }),
+                new OptimizeCssAssetsPlugin({
+                    assetNameRegExp: /\.css$/g,
+                    cssProcessor: cssnano,
+                    cssProcessorOptions: {
+                        discardComments: {
+                            removeAll: false,
+                        },
+                    },
+                    canPrint: true,
+                }),
             ],
         }
     }
